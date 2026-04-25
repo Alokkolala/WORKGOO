eeb070771c7f096aa4fcd3ccbb5c0470bd2df22b
Alokkolala <143516327+Alokkolala@users.noreply.github.com>
2026-04-25 13:59:17 +0500

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/platform/',
+  server: {
+    proxy: {
+      '/api': 'http://localhost:4242'
+    }
+  },
+  build: {
+    outDir: 'dist'
+  }
+})
